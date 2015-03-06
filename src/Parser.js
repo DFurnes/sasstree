fcae2b9f8c4ea508e17d7cb024e187fd13a9f3d4
David Furnes <david@dfurnes.com>
2015-03-05 22:35:34 -0500

    Properly attach whitespace after a parent node, rather than it's child.

--- src/Parser.js
+++ src/Parser.js
@@ -138,8 +138,11 @@ class Parser {
      * Unset current parent, moving back up towards the root of the AST.
      */
     unsetParent() {
-        if(this.current.parent) {
-            this.current = this.current.parent;
+        let newParent = this.current.parent;
+
+        if(newParent) {
+            this.current = newParent;
+            this.latest = newParent;
         }
     }
 
