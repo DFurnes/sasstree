06dfda012b97a239c865e34fd919cf27fbb99d4a
David Furnes <david@dfurnes.com>
2015-07-31 11:42:36 -0400

    Better error handling.

--- src/Console.js
+++ src/Console.js
@@ -14,14 +14,26 @@ program
   .option('-b, --bench', 'measure and output timing data')
   .parse(process.argv);
 
+console.log('');
+
+// File is a required parameter
+if(program.args.length === 0) {
+  console.error(`You must provide a source file. See 'sasstree -h' for details.`);
+  process.exit(1);
+}
 
 /**
  * Read input.
  */
-var file = program.args[0];
-var input = fs.readFileSync(path.resolve(file), 'utf-8');
+const file = path.resolve(program.args[0]);
 
-console.log('');
+// The file must exist!
+if(!fs.existsSync(file)) {
+  console.error(`Couldn't load '${file}'.`);
+  process.exit(1);
+}
+
+const input = fs.readFileSync(file, 'utf-8');
 
 /**
  * Parse into an AST!
@@ -30,8 +42,8 @@ if(program.bench) {
   console.time('SassTree');
 }
 
-let parser = new Parser();
-let ast = parser.parse(input, { bench: program.bench });
+const parser = new Parser();
+const ast = parser.parse(input, { bench: program.bench });
 
 if(program.bench) {
   console.timeEnd('SassTree');
